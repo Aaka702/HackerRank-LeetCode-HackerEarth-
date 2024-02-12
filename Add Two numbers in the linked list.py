348d44aba46769d24b6994904a0aae1bf97815eb
Aaka702 <140441760+Aaka702@users.noreply.github.com>
2024-02-12 19:30:01 +0530

    leetcode

---
+++ Add Two numbers in the linked list.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for singly-linked list.
+# Definition for singly-linked list.
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        dummy = ListNode(0)
+        current = dummy
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            carry, digit = divmod(val1 + val2 + carry, 10)
+            current.next = ListNode(digit)
+            current = current.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
