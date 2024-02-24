1f62dfc0ca8200fb6db943e62cbf6d7244527d07
Aaka702 <aakashputhussery.official@gmail.com>
2024-02-24 23:51:20 +0530

    Hacker Rank

--- time conversion.py
+++ time conversion.py
@@ -1,13 +1,28 @@
-t=input("")
-print(t)
-a=t[0]
-a=t.split(":")
-d=a[2:]
-e=0
-e=d
-#e=d.split("")
-print(e)
-print(d)
-c=a[:-1]
-print(c)
-print(a)
+my_list = input("")
+split_chars = []
+for string in my_list:
+    for char in string:
+        split_chars.append(char)
+print(split_chars)
+c=split_chars[:-2]
+balance=split_chars[2:8]
+print("time is ", balance)
+g=''.join(balance)
+print(g)
+if 'p,m' or 'P,M' in c:
+    d=c[0:2]
+    print(d)
+    concatenated_string = ''.join(d)
+    print(concatenated_string)
+    e=0
+    concatenated_string=int(e)
+    t = e  # Example string representing a number
+    t_as_int = int(t)  # Convert the string to an integer
+    result = t_as_int + 12
+    print(result)
+    list1=list(result).split(' ')
+    
+    converted_time=
+    print(converted_time)
+else:
+    print('false')
